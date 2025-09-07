a583dad3d0d6a2e885b9afdb9fb5dd89346588b8
shashwat <shashwatsharma2490@gmail.com>
2025-09-07 16:25:04 +0530

    this is my coomit for web application

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np 
 import pandas as pd
 
-app=Flask(_name_)
+app=Flask(__name__)
 regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -12,7 +13,15 @@ def home():
 
 @app.route('/predict_api',methods=['POST'])
 
-def predict_api();
+def predict_api():
     data=request.json['data']
     print(data)
-    
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
+
